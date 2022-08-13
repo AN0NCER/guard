4d2f50dc273ce1d34aa859e2d00027b9400836c0
Anoncer <anoncer@protonmail.com>
2022-08-13 23:52:29 +0200

    Added a static class responsible for Guard files. Edited the existing code related to writing files.

---
+++ Guard/Guard/Library/IO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace Guard.Library
+{
+    public static class IO
+    {
+        /// <summary>
+        /// Path to Folder save guard files
+        /// </summary>
+        public static string PathGuardFile { get; private set; } =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/acc/";
+
+        /// <summary>
+        /// Guard files Extension
+        /// </summary>
+        public static string ExtensionGuardFile { get; private set; } =
+            ".guard";
+
+        private static List<string> _files = null;
+
+        /// <summary>
+        /// Return Guard Files
+        /// </summary>
+        public static List<string> Files
+        {
+            get
+            {
+                if (_files == null)
+                    _files = GetFiles();
+                return _files;
+            }
+            set
+            {
+                _files = value;
+            }
+        }
+
+        private static List<string> GetFiles()
+        {
+            List<string> files = new List<string>();
+
+            if (!Directory.Exists(PathGuardFile))
+                Directory.CreateDirectory(PathGuardFile);
+
+            //Return files only specifd extension
+            files = Directory.GetFiles(PathGuardFile, $"*{ExtensionGuardFile}").ToList();
+
+            return files;
+        }
+
+        public static bool AddAndWrite(string name, string content)
+        {
+            if (!Directory.Exists(PathGuardFile))
+                Directory.CreateDirectory(PathGuardFile);
+
+            string file = Path.Combine(PathGuardFile, name + ".guard");
+            try
+            {
+                File.WriteAllText(file, content);
+
+                if (_files == null)
+                    _files = GetFiles();
+
+                _files.Add(file);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
+
