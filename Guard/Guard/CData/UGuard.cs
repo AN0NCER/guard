e7d3d61d541375bc8e4e66e5eb192c24e1f3a566
Anoncer <anoncer@protonmail.com>
2022-08-14 19:57:33 +0200

    Class UGuard

---
+++ Guard/Guard/CData/UGuard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SteamAuth;
+using System.ComponentModel;
+
+namespace Guard.CData
+{
+    public class UGuard : SteamGuardAccount, INotifyPropertyChanged
+    {
+        private string _secretCode = "00000";
+
+        private double _progressTime = 0f;
+
+        public string SecretCode
+        {
+            get => _secretCode;
+            set
+            {
+                _secretCode = value;
+                OnPropertyChanged(nameof(SecretCode));
+            }
+        }
+
+        public double ProgressTime
+        {
+            get => _progressTime;
+            set
+            {
+                _progressTime = value;
+                OnPropertyChanged(nameof(ProgressTime));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
