9128ad62e31118f5e5b9b11a98e7e7c715088f94
Anoncer <anoncer@protonmail.com>
2022-08-21 15:22:09 +0200

    Trade Window and List Box UI Controll Trades (Accept Decline and Click)

--- Guard/Guard/TradeView.xaml.cs
+++ Guard/Guard/TradeView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Guard.CData;
 using SteamAuth;
 using Xamarin.Forms;
@@ -8,16 +9,35 @@ namespace Guard
 {
     public partial class TradeView : ContentView
     {
-        SteamGuardAccount _guardAccount; // Guard Account 
+        SteamGuardAccount _guardAccount; // Guard Account
+
+
+        public ObservableCollection<User> Confirmations { get; set; } = new ObservableCollection<User>();
 
         public TradeView(SteamGuardAccount uGuard)
         {
             InitializeComponent();
+            this.BindingContext = this;
             _guardAccount = uGuard;
+            Confirmations.Add(new User { Name = "Anoncer", Image = "https://avatars.akamai.steamstatic.com/6bfb74e338765cf00d13d4b27473693c5e3c2ccf_full.jpg" });
+            Confirmations.Add(new User { Name = "Anoncer", Image = "https://avatars.akamai.steamstatic.com/6bfb74e338765cf00d13d4b27473693c5e3c2ccf_full.jpg" });
+            Confirmations.Add(new User { Name = "Anoncer", Image = "https://avatars.akamai.steamstatic.com/6bfb74e338765cf00d13d4b27473693c5e3c2ccf_full.jpg" });
 
-            var trades = _guardAccount.FetchConfirmations();
-            var key = _guardAccount.GetConfirmationTradeOfferID(trades[0]);
+            //_guardAccount.RefreshSession();
+            //var trades = _guardAccount.FetchConfirmations();
+            //var key = _guardAccount.GetConfirmationTradeOfferID(trades[0]);
         }
+
+        void ClickGestureRecognizer_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
     }
 }
 
