24748ca781153a6b73641ca78ea78056d2cc1b54
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-08-23 20:34:55 +0100

    GuradWidget

--- Guard/Guard.Android/WidgetService.cs
+++ Guard/Guard.Android/WidgetService.cs
@@ -2,57 +2,54 @@
 using Android.Appwidget;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Guard.Library;
 using Newtonsoft.Json;
 using SteamAuth;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
+
 
 namespace Guard.Droid
 {
-    [Service]
-    public class WidgetService : Service
+    [BroadcastReceiver]
+    public class WidgetService : BroadcastReceiver
     {
-        private SteamGuardAccount steamGuard;
-        public override void OnStart(Intent intent, int startId)
+        private static SteamGuardAccount steamGuard;
+
+        public override void OnReceive(Context context, Intent intent)
         {
             // Build the widget update for today
-            //RemoteViews updateViews = buildUpdate(this);
+            RemoteViews updateViews = buildUpdate(context);
 
-            //// Push update for this widget to the home screen
-            //ComponentName thisWidget = new ComponentName(this, Java.Lang.Class.FromType(typeof(GuardWidgetClass)).Name);//"guard.GuardWidgetClass"
-            //AppWidgetManager manager = AppWidgetManager.GetInstance(this);
-            //manager.UpdateAppWidget(thisWidget, updateViews);
+            // Push update for this widget to the home screen
+            ComponentName thisWidget = new ComponentName(context, Java.Lang.Class.FromType(typeof(GuardWidgetClass)).Name);//"guard.GuardWidgetClass"
+            AppWidgetManager manager = AppWidgetManager.GetInstance(context);
+            manager.UpdateAppWidget(thisWidget, updateViews);
+            Toast.MakeText(context, "UpDate", ToastLength.Short).Show();//Test
+            Restart(context);
         }
 
-        public override IBinder OnBind(Intent intent)
-		{
-			// We don't need to bind to this service
-			return null;
-		}
+        void Restart(Context context)
+        {
+            Intent intent = new Intent(context, typeof(WidgetService));
+            PendingIntent pending = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.Mutable);
+            AlarmManager alarm = (AlarmManager)context.GetSystemService(Context.AlarmService);
+            alarm.Cancel(pending);
+            alarm.SetRepeating(AlarmType.Rtc, SystemClock.ElapsedRealtime(), 30000, pending);
 
-        //Build a widget update to show the current Wiktionary
-         //"Word of the day." Will block until the online API returns.
+            
+        }
         public RemoteViews buildUpdate(Context context)
         {
-
             if (steamGuard == null)
             {
                 List<string> files = IO.Files;
                 steamGuard = JsonConvert.DeserializeObject<SteamGuardAccount>(File.ReadAllText(files[0]));
             }
-
             string code = steamGuard.GenerateSteamGuardCode();
-
-            RemoteViews views = new RemoteViews(this.PackageName, Resource.Layout.guardwidget);
+            RemoteViews views = new RemoteViews(context.PackageName, Resource.Layout.guardwidget);
             views.SetTextViewText(Resource.Id.Code, code);
-
             return views;
         }
 
