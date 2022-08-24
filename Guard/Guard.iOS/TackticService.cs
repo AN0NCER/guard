88f9a74c81d32a4f5cddd2a8ea45f0c8e472124d
Anoncer <anoncer@protonmail.com>
2022-08-24 18:27:21 +0200

    Service Tacktile Vibration iOS

---
+++ Guard/Guard.iOS/TackticService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Guard;
+using Guard.iOS;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(TackticService))]
+namespace Guard.iOS
+{
+    public class TackticService : ITacktile
+    {
+        public void Tacktile()
+        {
+            // Initialize feedback
+            var impact = new UIImpactFeedbackGenerator(UIImpactFeedbackStyle.Light);
+            impact.Prepare();
+
+            // Trigger feedback
+            impact.ImpactOccurred();
+        }
+    }
+}
+
