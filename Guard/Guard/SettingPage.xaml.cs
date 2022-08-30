4d41f0b50e003f39773abd1d4199650e98512aa1
Anoncer <anoncer@protonmail.com>
2022-08-31 01:32:35 +0200

    Add setting page

---
+++ Guard/Guard/SettingPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Guard
+{
+    public partial class SettingPage : ContentPage
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
